f9799958e792a938bc114fbda775cbfebd0a50ef
faroukaf <faroukafmsm1@gmail.com>
2024-05-03 11:18:15 +0300

    user model

--- db/models.py
+++ db/models.py
@@ -13,4 +13,12 @@ class Blog(Base):
 
 
 
+class User(Base):
+  __tablename__ = 'user'
+
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String)
+  email = Column(String)
+  password = Column(String)
+
 
