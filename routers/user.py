29ad260e05472933de6cdd04b1b46b837ce6a4a6
faroukaf <faroukafmsm1@gmail.com>
2024-05-06 01:25:37 +0300

    add user repository

--- routers/user.py
+++ routers/user.py
@@ -7,6 +7,7 @@ from schema import user
 from db import models
 from db.db import get_db
 from util.hashing import Hash
+from .repository import user as ur
 
 
 
@@ -25,13 +26,7 @@ def create_user(
   my_request: user.User,
   db: Session = Depends(get_db)
 ):
-  inp = my_request.model_dump()
-  inp['password'] = Hash.bcrypt(inp['password'])
-  new_user = models.User(**inp)
-  db.add(new_user)
-  db.commit()
-  db.refresh(new_user)
-  return new_user
+  return ur.create(my_request, db)
 
 @router.get(
   '/{user_id}',
@@ -42,14 +37,7 @@ def show_user(
   user_id: str,
   db: Session = Depends(get_db)
 ):
-  user = db.query(models.User).filter(models.User.id == user_id).first()
-  if not user:
-    raise HTTPException(
-      status_code=status.HTTP_404_NOT_FOUND,
-      detail=f'User with id {user_id} not exist'
-    )
-
-  return user
+  return ur.show(user_id, db)
 
 
 
